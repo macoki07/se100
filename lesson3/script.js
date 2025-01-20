9e0cc24a4a52ccb2d6b855dd2ef526bc880159a1
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-20 19:57:21 +0800

    Ex 8

--- lesson3/script.js
+++ lesson3/script.js
@@ -30,6 +30,30 @@
 // const avgPrice = (parseFloat(DayOneClosingPrice) + parseFloat(DayTwoClosingPrice)) / 2;
 // console.log(avgPrice);
 
-const TSLAClosingPrices = [230.12, 221.45, 225.61, 219.48, 227.52];
-teslaAvgClosingPrice = (TSLAClosingPrices[0] + TSLAClosingPrices[1] + TSLAClosingPrices[2] + TSLAClosingPrices[3] + TSLAClosingPrices[4]) / 5;
-console.log(teslaAvgClosingPrice);
+// const TSLAClosingPrices = [230.12, 221.45, 225.61, 219.48, 227.52];
+// teslaAvgClosingPrice = (TSLAClosingPrices[0] + TSLAClosingPrices[1] + TSLAClosingPrices[2] + TSLAClosingPrices[3] + TSLAClosingPrices[4]) / 5;
+// console.log(teslaAvgClosingPrice);
+
+const AAPLCurrentBar = {
+	open: 81.12,
+	high: 82.59,
+	low: 80.09,
+	close: 84.71
+}
+
+const AAPLPrevBar = {
+	open: 81.02,
+	high: 82.49,
+	low: 79.09,
+	close: 83.71
+}
+
+const openPrice = (AAPLPrevBar.open +AAPLPrevBar.close)/2;
+const closePrice = (AAPLCurrentBar.open +AAPLCurrentBar.close +AAPLCurrentBar.high+AAPLCurrentBar.low)/4;
+const highPrice = Math.max(AAPLCurrentBar.high, AAPLCurrentBar.open, AAPLCurrentBar.close);
+const lowPrice = Math.min(AAPLCurrentBar.low, AAPLCurrentBar.open, AAPLCurrentBar.close);
+
+console.log(openPrice);
+console.log(closePrice);
+console.log(highPrice);
+console.log(lowPrice);
