af1b8b014b517c937c439494a323d8539f9f9de9
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-20 19:48:38 +0800

    Ex 7

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,31 +1,35 @@
 // let stockprice = 300;
 // console.log(stockprice);
 
-// let money = 6000
-// money += 3000
+// let money = 6000;
+// money += 3000;
 // console.log(money)
-// money *=3
+// money *=3;
 // console.log(money)
 
-// let company = "Micrsoft's" // Use the correct quotation
+// let company = "Micrsoft's"; // Use the correct quotation
 // console.log(company);
 
-// company = 'Micrsoft\'s' // Use escape sequences
+// company = 'Micrsoft\'s'; // Use escape sequences
 // console.log(company);
 
-// const companyName = "Amazon"
-// const companyRating = 5
+// const companyName = "Amazon";
+// const companyRating = 5;
 
-// const companyInfo = companyName + " has a customer satisfaction rating of " + companyRating  // Use string concatenation
+// const companyInfo = companyName + " has a customer satisfaction rating of " + companyRating;  // Use string concatenation
 // console.log(companyInfo);
 
-// const companyName = "Metaa"
+// const companyName = "Metaa";
 
 // console.log(companyName.substring(0, 4)) // Use the substring method
 // console.log(companyName.substring(0,4).toUpperCase()) // Use the toUpperCase method
 
-const DayOneClosingPrice = "87.14";
-const DayTwoClosingPrice = "84.22";
+// const DayOneClosingPrice = "87.14";
+// const DayTwoClosingPrice = "84.22";
 
-const avgPrice = (parseFloat(DayOneClosingPrice) + parseFloat(DayTwoClosingPrice)) / 2;
-console.log(avgPrice);
+// const avgPrice = (parseFloat(DayOneClosingPrice) + parseFloat(DayTwoClosingPrice)) / 2;
+// console.log(avgPrice);
+
+const TSLAClosingPrices = [230.12, 221.45, 225.61, 219.48, 227.52];
+teslaAvgClosingPrice = (TSLAClosingPrices[0] + TSLAClosingPrices[1] + TSLAClosingPrices[2] + TSLAClosingPrices[3] + TSLAClosingPrices[4]) / 5;
+console.log(teslaAvgClosingPrice);
