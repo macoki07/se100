10bdd19de0a70d36585e010ea5f3f7c354c9ecb2
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-20 19:28:10 +0800

    bonus ex 1

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,8 +1,14 @@
 // let stockprice = 300;
 // console.log(stockprice);
 
-let money = 6000
-money += 3000
-console.log(money)
-money *=3
-console.log(money)
+// let money = 6000
+// money += 3000
+// console.log(money)
+// money *=3
+// console.log(money)
+
+let company = "Micrsoft's" // Use the correct quotation
+console.log(company);
+
+company = 'Micrsoft\'s' // Use escape sequences
+console.log(company);
