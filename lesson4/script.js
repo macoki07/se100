5a8c9100b687c76656ed87198f944b1f8da0a8a6
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-27 19:20:50 +0800

    ex 2

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,8 +1,35 @@
-const MSFTStockClosingPrices = [254.12, 253.12, 256.97, 257.52, 258.93];
+// const MSFTStockClosingPrices = [254.12, 253.12, 256.97, 257.52, 258.93];
 
-// Write your code below
-let sum = 0;
-for (let i = 0; i < MSFTStockClosingPrices.length; i++) {
-    sum += MSFTStockClosingPrices[i];    
-}
-console.log("The total sum of closing prices of MSFT is " + sum );
+// // Write your code below
+// let sum = 0;
+// for (let i = 0; i < MSFTStockClosingPrices.length; i++) {
+//     sum += MSFTStockClosingPrices[i];
+// }
+// console.log("The total sum of closing prices of MSFT is " + sum );
+
+// const MSFTStockClosingPrices = [
+//   {
+//     Monday: 254.12,
+//     Tuesday: 253.12,
+//     Wednesday: 256.97,
+//     Thursday: 257.52,
+//     Friday: 258.93,
+//   },
+//   {
+//     Monday: 292.2,
+//     Tuesday: 289.73,
+//     Wednesday: 291.04,
+//     Thursday: 294.55,
+//     Friday: 296.29,
+//   },
+// ];
+
+// // Write your code below
+// let sum = 0;
+// for (let i = 0; i < MSFTStockClosingPrices.length; i++) {
+//     let week = MSFTStockClosingPrices[i];
+//     for (day in week){
+//         sum += week[day];
+//     }
+// }
+// console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2) );
