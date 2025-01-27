c368c2bfa44c78123427728bee87ce1b49fff7d3
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-27 19:45:20 +0800

    4

--- lesson4/script.js
+++ lesson4/script.js
@@ -64,3 +64,36 @@
 //     }
 // }
 // console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2) );
+
+const MSFTStockClosingPrices = [
+  {
+    Monday: 254.12,
+    Tuesday: 253.12,
+    Wednesday: 256.97,
+    Thursday: 257.52,
+    Friday: 258.93,
+  },
+  {
+    Monday: 292.2,
+    Tuesday: 289.73,
+    Wednesday: 291.04,
+    Thursday: 294.55,
+    Friday: 296.29,
+  },
+];
+
+// Write your code below
+let i = 0;
+let sum = 0;
+while (i < MSFTStockClosingPrices.length) {
+  let week = MSFTStockClosingPrices[i];
+  let days = Object.keys(week);
+  let j = 0;
+  while (j < days.length) {
+    sum += week[days[j]];
+    j++;
+  }
+  i++;
+}
+
+console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2));
