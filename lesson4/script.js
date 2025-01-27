46f87ee1225ef4fac70111146e6567784c2391fd
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-27 19:08:40 +0800

    Ex 1

---
+++ lesson4/script.js
@@ -0,0 +1,8 @@
+const MSFTStockClosingPrices = [254.12, 253.12, 256.97, 257.52, 258.93];
+
+// Write your code below
+let sum = 0;
+for (let i = 0; i < MSFTStockClosingPrices.length; i++) {
+    sum += MSFTStockClosingPrices[i];    
+}
+console.log("The total sum of closing prices of MSFT is " + sum );
