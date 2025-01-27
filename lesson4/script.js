441f0ee1ad1e39a0343b88bfda171ec39e2ebc78
macoki07 <70043062+macoki07@users.noreply.github.com>
2025-01-27 19:49:26 +0800

    l4e5

--- lesson4/script.js
+++ lesson4/script.js
@@ -65,35 +65,54 @@
 // }
 // console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2) );
 
-const MSFTStockClosingPrices = [
-  {
-    Monday: 254.12,
-    Tuesday: 253.12,
-    Wednesday: 256.97,
-    Thursday: 257.52,
-    Friday: 258.93,
-  },
-  {
-    Monday: 292.2,
-    Tuesday: 289.73,
-    Wednesday: 291.04,
-    Thursday: 294.55,
-    Friday: 296.29,
-  },
-];
+// const MSFTStockClosingPrices = [
+//   {
+//     Monday: 254.12,
+//     Tuesday: 253.12,
+//     Wednesday: 256.97,
+//     Thursday: 257.52,
+//     Friday: 258.93,
+//   },
+//   {
+//     Monday: 292.2,
+//     Tuesday: 289.73,
+//     Wednesday: 291.04,
+//     Thursday: 294.55,
+//     Friday: 296.29,
+//   },
+// ];
 
-// Write your code below
-let i = 0;
-let sum = 0;
-while (i < MSFTStockClosingPrices.length) {
-  let week = MSFTStockClosingPrices[i];
-  let days = Object.keys(week);
-  let j = 0;
-  while (j < days.length) {
-    sum += week[days[j]];
-    j++;
+// // Write your code below
+// let i = 0;
+// let sum = 0;
+// while (i < MSFTStockClosingPrices.length) {
+//   let week = MSFTStockClosingPrices[i];
+//   let days = Object.keys(week);
+//   let j = 0;
+//   while (j < days.length) {
+//     sum += week[days[j]];
+//     j++;
+//   }
+//   i++;
+// }
+
+// console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2));
+
+function getBetween() {
+  const MSFTStockClosingPrices = [254.12, 253.12, 256.97, 257.52, 258.93];
+
+  // Initialize a counter for prices between 254 and 257
+  let count = 0;
+
+  // Loop through each price in the array
+  for (let i = 0; i < MSFTStockClosingPrices.length; i++) {
+    if (MSFTStockClosingPrices[i] > 254 && MSFTStockClosingPrices[i] < 257) {
+      count++;
+    }
   }
-  i++;
+
+  // Log the result in the console
+  console.log(`Number of closing prices between 254 and 257: ${count}`);
 }
 
-console.log("The total sum of closing prices of MSFT is " + sum.toFixed(2));
+getBetween();
